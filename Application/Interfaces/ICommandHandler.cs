80680785601b8c61f0f639157f9b464db6996e57
Herbert Carvalho <herbertcarvalho18@gmail.com>
2026-01-15 22:01:42 -0300

    adjust aplication to call validation

--- Application/Interfaces/ICommandHandler.cs
+++ Application/Interfaces/ICommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Application.Interfaces;
 
@@ -14,8 +15,16 @@ public class CommandDispatcher(IServiceProvider serviceProvider)
     public async Task<TResult> DispatchAsync<TCommand, TResult>(TCommand command, CancellationToken ct = default)
         where TCommand : ICommand<TResult>
     {
+        var validator = serviceProvider.GetService<IValidator<TCommand>>();
+        if (validator != null)
+        {
+            var validationResult = await validator.ValidateAsync(command, ct);
+            if (!validationResult.IsValid)
+                throw new ValidationException(validationResult.Errors);
+        }
+
         var handlerType = typeof(ICommandHandler<,>).MakeGenericType(typeof(TCommand), typeof(TResult));
-        var handler = serviceProvider.GetServices(handlerType);
+        var handler = serviceProvider.GetService(handlerType);
 
         var method = handlerType.GetMethod("HandleAsync");
         var task = (Task<TResult>)method!.Invoke(handler, [command, ct])!;
---
+++ Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Application.Commands.CreateReservation;
+using Application.Common;
+using Application.Interfaces;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddCommands(this IServiceCollection Services)
+    {
+        Services.AddScoped<ICommandHandler<CreateReservationCommand, ApiResponse<Guid>>, CreateReservationHandler>();
+    }
+
+    public static IServiceCollection AddValidation(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        return services;
+    }
+}
