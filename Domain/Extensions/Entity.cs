efb64878269e4e9d741df718ae38f382853f0f15
Herbert Carvalho <herbertcarvalho18@gmail.com>
2026-01-15 22:02:03 -0300

    Fix entity datetime needs to be utc

--- Domain/Extensions/Entity.cs
+++ Domain/Extensions/Entity.cs
@@ -4,7 +4,7 @@
     {
         protected Entity()
         {
-            CreatedAt = DateTime.Now;
+            CreatedAt = DateTime.UtcNow;
         }
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
