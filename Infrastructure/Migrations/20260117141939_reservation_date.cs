f9558c636705ea582348945f7b23c625b8c5c983
Herbert Carvalho <herbertcarvalho18@gmail.com>
2026-01-17 11:20:33 -0300

    adding migration of new Column ReservationDate in Table Reservation

---
+++ Infrastructure/Migrations/20260117141939_reservation_date.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class reservation_date : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "reservation_date",
+                table: "reservation",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "reservation_date",
+                table: "reservation");
+        }
+    }
+}
